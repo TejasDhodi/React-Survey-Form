f674f176f16f8b58aa3c0c288b83c7756da7cae0
Tejas Dhodi <tejasdhodi77@gmail.com>
2023-07-21 22:53:01 +0530

    updated

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -12,7 +12,7 @@ const ThankYouPage = ({ redirectToMainPage }) => {
   }, [redirectToMainPage]);
 
   return (
-    <div>
+    <div className='thank_you'>
       <h2>Thank you for your submission!</h2>
       <p>You are beign redirected to main page...</p>
     </div>
